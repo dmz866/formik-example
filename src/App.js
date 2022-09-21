d1a7bdb28350e0a96a22e39a1fa837802b22895d
David Murillo <david.murillo@ascendle.com>
2022-09-21 18:31:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { Formik } from 'formik';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+	const validateFields = (values) => {
+		const errors = {};
 
-export default App;
+		if (!values.name) {
+			errors.name = 'Requerido';
+		}
+		else if (values.name.length < 5) {
+			errors.name = 'El nombre es muy corto';
+		}
+
+		if (!values.lastName) {
+			errors.lastName = 'Requerido';
+		}
+		else if (values.lastName.length < 5) {
+			errors.lastName = 'El nombre es muy corto';
+		}
+
+		return errors;
+	}
+	/*
+		const formik = useFormik({
+			initialValues: {
+				name: '',
+				lastName: '',
+				email: '',
+			},
+			validate: validateFields,
+			onSubmit: (values) => { console.log(values) }
+		});
+	*/
+	return (
+		<Formik initialValues={{ name: '', lastName: '', email: '' }} validate={validateFields} onSubmit={(values) => { console.log(values) }}>
+			{(formik) =>
+				<form onSubmit={formik.handleSubmit}>
+					<label>Nombre</label>
+					<input {...formik.getFieldProps('name')} type='text'></input>
+					{(formik.touched.name && formik.errors.name) ? <div>{formik.errors.name}</div> : null}
+					<br />
+					<label>Apellido</label>
+					<input type='text'{...formik.getFieldProps('lastName')} ></input>
+					{(formik.touched.lastName && formik.errors.lastName) ? <div>{formik.errors.lastName}</div> : null}
+					<br />
+					<label>Correo</label>
+					<input type='email' {...formik.getFieldProps('email')} ></input>
+					{(formik.touched.email && formik.errors.email) ? <div>{formik.errors.email}</div> : null}
+					<button type='submit'>Enviar</button>
+				</form>
+			}
+		</Formik >
+	);
+}
